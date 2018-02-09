eaa2f8687a27896acdd7198f222b6b5b57b66e8b
Luzi82 <luzi82@gmail.com>
2018-02-09 23:33:18 +0800

    fix python3

--- census/keras/trainer/task.py
+++ census/keras/trainer/task.py
@@ -145,8 +145,8 @@ def dispatch(train_files,
 
 # h5py workaround: copy local models over to GCS if the job_dir is GCS.
 def copy_file_to_gcs(job_dir, file_path):
-  with file_io.FileIO(file_path, mode='r') as input_f:
-    with file_io.FileIO(os.path.join(job_dir, file_path), mode='w+') as output_f:
+  with file_io.FileIO(file_path, mode='rb') as input_f:
+    with file_io.FileIO(os.path.join(job_dir, file_path), mode='w+b') as output_f:
         output_f.write(input_f.read())
 
 if __name__ == "__main__":
